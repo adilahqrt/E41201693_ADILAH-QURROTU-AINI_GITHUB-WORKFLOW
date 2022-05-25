369d31837f502de467fbacc8b1426c2b257bf56e
Adilah Qurrotu'aini <33256041+adilahqrt@users.noreply.github.com>
2022-05-25 10:44:59 +0700

    add main.dart

---
+++ learn_auth/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:learn_auth/LoginScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyAUkyDjgHgBy0sG0N0bVse7TIJMgFGGY0Q',
+          appId: '1:119797710240:android:0039529ec4cfb1021678c5',
+          messagingSenderId: '119797710240',
+          projectId: 'login-64753'));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
