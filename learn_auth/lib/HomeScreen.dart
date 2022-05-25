83c634a777c4886653bf9ecd008e30da87e6a0b1
Adilah Qurrotu'aini <33256041+adilahqrt@users.noreply.github.com>
2022-05-25 11:14:25 +0700

    update homeScreen.dart

--- learn_auth/lib/HomeScreen.dart
+++ learn_auth/lib/HomeScreen.dart
@@ -22,7 +22,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -53,72 +53,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-//   @override
-//   Widget build(BuildContext context) {
-//     FirebaseAuth auth = FirebaseAuth.instance;
-//     if (auth.currentUser != null) {
-//       print(auth.currentUser!.email);
-//     }
-//     return Scaffold(
-//       body: Padding(
-//           padding: const EdgeInsets.all(30.0),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               const SizedBox(height: 60),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 children: [
-//                   IconButton(
-//                     icon: const Icon(Icons.notifications),
-//                     onPressed: () {},
-//                   ),
-//                   IconButton(
-//                     icon: const Icon(Icons.extension),
-//                     onPressed: () {},
-//                   ),
-//                 ],
-//               ),
-//               const SizedBox(height: 37),
-//               Text.rich(
-//                 TextSpan(children: <TextSpan>[
-//                   TextSpan(
-//                     text: "Welcome, \n",
-//                     style: TextStyle(color: Colors.blue[300]),
-//                   ),
-//                   TextSpan(
-//                     text: auth.currentUser!.email,
-//                     style: TextStyle(color: Colors.blue[900]),
-//                   ),
-//                 ]),
-//                 style: const TextStyle(fontSize: 30),
-//               ),
-//               const SizedBox(height: 20),
-//               TextField(
-//                 decoration: InputDecoration(
-//                     prefixIcon: const Icon(Icons.search, size: 18),
-//                     border: OutlineInputBorder(
-//                         borderRadius: BorderRadius.circular(10)),
-//                     hintText: 'Search'),
-//               ),
-//               const SizedBox(height: 10),
-//               const Text(
-//                 "Recomended Place",
-//                 style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-//               ),
-//               Container(
-//                 child: ElevatedButton(
-//                     onPressed: () {
-//                       _signOut().then((value) => Navigator.of(context)
-//                           .pushReplacement(MaterialPageRoute(
-//                               builder: (context) => const LoginScreen())));
-//                     },
-//                     child: const Text('Logout')),
-//               ),
-//             ],
-//           )),
-//     );
-//   }
-// }
-
-//final countries = ["Tokyo", "Berlin", "Roma", "Monas"];
